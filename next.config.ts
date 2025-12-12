85fe5b9e79c4b3b9f4bb249007a0d9040f2aef3a
wangtao <wangtao@pzds.com>
2025-12-12 09:54:26 +0800

    feat: 配置修改

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 静态导出，适配 4everland
-  images: {
-    unoptimized: true, // 静态导出不支持图片优化
-  },
+  /* config options here */
 };
 
 export default nextConfig;
